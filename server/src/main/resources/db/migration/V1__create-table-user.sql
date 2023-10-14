b12a08c9e75a6feaeed5ba0e631365e3a90658b2
musagy <diegomusagy@gmail.com>
2023-10-13 22:49:45 -0500

    Fix(User): SQL query without commas when creating the table ✅

--- server/src/main/resources/db/migration/V1__create-table-user.sql
+++ server/src/main/resources/db/migration/V1__create-table-user.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user (
-    id bigint NOT NULL AUTO_INCREMENT
-    email varchar(100) NOT NULL UNIQUE
-    username varchar(100) NOT NULL UNIQUE
-    userAlias varchar(100)
-    password varchar(100) NOT NULL
+    id bigint NOT NULL AUTO_INCREMENT,
+    email varchar(100) NOT NULL UNIQUE,
+    username varchar(100) NOT NULL UNIQUE,
+    userAlias varchar(100),
+    password varchar(100) NOT NULL,
 
     PRIMARY KEY(id)
 )
