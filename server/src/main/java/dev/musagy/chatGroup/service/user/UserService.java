e58ab9404ab87a2be57b2baca1c11951cbdf3556
musagy <diegomusagy@gmail.com>
2023-10-15 11:07:52 -0500

    fix(userService): Limit UserService based on UserRepo to only have methods related to user (remove the jwt, and only create user) 🐱‍🐉

--- server/src/main/java/dev/musagy/chatGroup/service/user/UserService.java
+++ server/src/main/java/dev/musagy/chatGroup/service/user/UserService.java
@@ -1,8 +1,17 @@
 package dev.musagy.chatGroup.service.user;
 
+import dev.musagy.chatGroup.model.user.Role;
 import dev.musagy.chatGroup.model.user.SignUpRequest;
-import dev.musagy.chatGroup.model.user.SignUpResponse;
+import dev.musagy.chatGroup.model.user.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface UserService {
-    SignUpResponse signUp(SignUpRequest req);
+    User saveUser(SignUpRequest req);
+
+    Optional<User> findByUsername(String username);
+
+    @Transactional
+    void changeRole(Role newRole, String username);
 }
--- server/src/main/java/dev/musagy/chatGroup/service/user/UserServiceImpl.java
+++ server/src/main/java/dev/musagy/chatGroup/service/user/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package dev.musagy.chatGroup.service.user;
 
+import dev.musagy.chatGroup.model.user.Role;
 import dev.musagy.chatGroup.model.user.SignUpRequest;
-import dev.musagy.chatGroup.model.user.SignUpResponse;
 import dev.musagy.chatGroup.model.user.User;
 import dev.musagy.chatGroup.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
     @Autowired
@@ -16,12 +19,21 @@ public class UserServiceImpl implements UserService {
 //    private JwtP
 
     @Override
-    public SignUpResponse signUp(SignUpRequest req) {
+    public User saveUser(SignUpRequest req) {
         String passwordEncrypt = passwordEncoder.encode(req.password());
-        var newUser = userRepository.save(
-                new User(req, passwordEncrypt)
-        );
-        String jwt = "bearer hola_chupapis";
-        return new SignUpResponse(newUser, jwt);
+
+        return userRepository
+                .save(new User(req, passwordEncrypt));
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Transactional
+    @Override
+    public void changeRole(Role newRole, String username){
+        userRepository.updateUserRole( username, newRole);
     }
 }
