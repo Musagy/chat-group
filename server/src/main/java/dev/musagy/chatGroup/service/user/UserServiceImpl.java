9e14cf43252551caf61a2b650d46ad43778c8c38
musagy <diegomusagy@gmail.com>
2023-10-16 00:18:48 -0500

    fix(UserService): Add annotation @Service 👻

--- server/src/main/java/dev/musagy/chatGroup/service/user/UserServiceImpl.java
+++ server/src/main/java/dev/musagy/chatGroup/service/user/UserServiceImpl.java
@@ -6,10 +6,12 @@ import dev.musagy.chatGroup.model.user.User;
 import dev.musagy.chatGroup.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
