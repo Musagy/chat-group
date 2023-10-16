3a5c987b6f3c86aec8da60e84ca122a104d65e43
musagy <diegomusagy@gmail.com>
2023-10-16 00:27:36 -0500

    Feat(UserDetail/UserPrincipal): Add CustomUserDetailService and UserPrincipal 🔑

---
+++ server/src/main/java/dev/musagy/chatGroup/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package dev.musagy.chatGroup.security;
+
+import dev.musagy.chatGroup.model.user.User;
+import dev.musagy.chatGroup.service.user.UserService;
+import dev.musagy.chatGroup.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("el usuario no fue encontrado: " + username)
+        );
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils
+                .convertToAuthority(user.getRole().name())
+        );
+
+        return UserPrincipal.builder()
+                .user(user)
+                .id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}
