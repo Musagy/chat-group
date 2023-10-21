fc7c4cf5c85ae8967ff18ca53f32006d99f52392
musagy <diegomusagy@gmail.com>
2023-10-21 11:10:51 -0500

    fix(SpringSecurity): Remove User param in response for security 👍🏿 🐱‍👤

--- server/src/main/java/dev/musagy/chatGroup/model/user/SignInOrSignUpResponse.java
+++ server/src/main/java/dev/musagy/chatGroup/model/user/SignInOrSignUpResponse.java
@@ -1,11 +1,8 @@
 package dev.musagy.chatGroup.model.user;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record SignInOrSignUpResponse(
-    @NotNull
-    User user,
     @NotBlank
     String jwt
 ) {
